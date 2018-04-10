f2516a16ec012ba2e371d1c5b0ef6463d301fe9f
Nick Ellsworth <nellsworth@squarespace.com>
2018-04-10 14:37:31 -0400

    fix edge case where app would crash if you added a song to an empty room

--- src/scripts/app/main/Player.js
+++ src/scripts/app/main/Player.js
@@ -8,42 +8,38 @@ import { initPlayer, playSong, getCurrentSong } from '../actions/spotifyAPI'
 class Player extends React.Component {
   constructor(props) {
     super(props)
-    this.onClickPlayPause = this.onClickPlayPause.bind(this)
     this.state = {
-      ready: false,
+      playerIsLoaded: false,
     }
   }
 
   async componentWillMount() {
     await this.props.initPlayer()
     this.setState({
-      ready: true,
+      playerIsLoaded: true,
     })
     const currentSong = this.props.room.queue[0]
     this.props.playSong(currentSong.uri)
   }
 
   componentWillReceiveProps(nextProps) {
-    if (!this.state.ready) return
+    if (!this.state.playerIsLoaded) return
     if (!nextProps.room || !nextProps.room.queue) return
 
     // dont play a new song if the first tracks are the same, for now.
-    if (this.props.room.queue[0].uri === nextProps.room.queue[0].uri) return
+    // we should revisit this
+
+    if (this.props.room.queue && (this.props.room.queue[0].uri === nextProps.room.queue[0].uri)) return
     const currentSong = nextProps.room.queue[0]
     this.props.playSong(currentSong.uri)
   }
 
-  onClickPlayPause() {
-    this.props.player.togglePlay()
-  }
-
   render() {
-    if (!this.state.ready) return null
+    if (!this.state.playerIsLoaded) return null
     if (!this.props.room || !this.props.room.queue) return null
     return (
       <React.Fragment>
         <div>Currently playing: {this.props.room.queue[0].name}</div>
-        <button onClick={this.onClickPlayPause}>Play/Pause</button>
       </React.Fragment>
     )
   }
